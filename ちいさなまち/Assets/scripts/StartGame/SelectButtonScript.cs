8bc7131667339eadfd63d57f806ab153ef0c40ef
SHO800 <86473181+SHO800@users.noreply.github.com>
2022-08-23 15:53:34 +0900

    StartGameシーンを更新
    サイズを暫定的に1024×576に変更、これに伴いボタン等のサイズも調整

---
+++ ちいさなまち/Assets/scripts/StartGame/SelectButtonScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectButtonScript : MonoBehaviour
+{
+    public GameObject changeTarget;
+
+    public void OnClick()
+    {
+        foreach (Transform childObject in changeTarget.transform){
+            childObject.gameObject.SetActive(false);
+        }
+
+        int mySelfIndex = transform.GetSiblingIndex();
+        Transform target = changeTarget.transform.GetChild(mySelfIndex);
+        target.gameObject.SetActive(true);
+    }
+}
