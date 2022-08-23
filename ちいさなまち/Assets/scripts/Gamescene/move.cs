02e32ee23be5f6a366333698a7aca8cbc306ef77
tarou652 <rrrr20050307@gmail.com>
2022-08-23 14:04:51 +0900

    アニメーションの修正

--- ちいさなまち/Assets/scripts/Gamescene/move.cs
+++ ちいさなまち/Assets/scripts/Gamescene/move.cs
@@ -5,6 +5,7 @@ using TMPro;
 // MonoBehaviourPunCallbacksを継承して、photonViewプロパティを使えるようにする
 public class move : MonoBehaviourPunCallbacks
 {
+    private bool ispush;
     private int idX = Animator.StringToHash("x"), idY = Animator.StringToHash("y");
     private Animator animator = null;
     public static Vector3 popo;
@@ -28,11 +29,11 @@ public class move : MonoBehaviourPunCallbacks
         {
             float x = Input.GetAxisRaw("Horizontal");
             float y = Input.GetAxisRaw("Vertical");
-            if (Input.GetAxisRaw("Horizontal") != 0)
+            if (Input.GetAxisRaw("Horizontal") != 0 )
             {
                 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0f);
             }
-            else if(Input.GetAxisRaw("Vertical") != 0)
+            else if(Input.GetAxisRaw("Vertical") != 0 )
                 {
                 input = new Vector3(0, Input.GetAxisRaw("Vertical"), 0f);
             }
@@ -42,14 +43,14 @@ public class move : MonoBehaviourPunCallbacks
             }
             transform.Translate(5f * Time.deltaTime * input.normalized);
             popo = transform.position;
-            if (Input.GetKeyUp(KeyCode.D)|| Input.GetKeyUp(KeyCode.RightArrow)){ animator.SetFloat(idX, 0.5f); animator.SetFloat(idY, 0); }
-            if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)) { animator.SetFloat(idX, -0.5f); animator.SetFloat(idY, 0); }
-            if (x > 0.1) { animator.SetFloat(idX, 1); }
-            if (x <-0.1) { animator.SetFloat(idX, -1); }
-            if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)) { animator.SetFloat(idY, 0.5f); animator.SetFloat(idX, 0); }
-            if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)) { animator.SetFloat(idY, -0.5f); animator.SetFloat(idX, 0); }
-            if (y > 0.1) { animator.SetFloat(idY, 1); }
-            if (y < -0.1) { animator.SetFloat(idY, -1); }
+            if (Input.GetKeyUp(KeyCode.D)|| Input.GetKeyUp(KeyCode.RightArrow)){ animator.SetFloat(idX, 0.5f); animator.SetFloat(idY, 0); ispush = false; }
+            if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)) { animator.SetFloat(idX, -0.5f); animator.SetFloat(idY, 0); ispush=false;}
+            if (x > 0.1 && ispush == false) { animator.SetFloat(idX, 1); ispush = true; }
+            if (x <-0.1 && ispush == false) { animator.SetFloat(idX, -1); ispush = true; }
+            if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)) { animator.SetFloat(idY, 0.5f); animator.SetFloat(idX, 0); ispush = false; }
+            if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)) { animator.SetFloat(idY, -0.5f); animator.SetFloat(idX, 0); ispush = false; }
+            if (y > 0.1 && ispush == false) { animator.SetFloat(idY, 1); ispush = true; }
+            if (y < -0.1 && ispush == false) { animator.SetFloat(idY, -1); ispush = true; }
         }
     }
    
