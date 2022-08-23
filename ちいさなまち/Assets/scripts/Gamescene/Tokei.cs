56304aadd3ba55c079a7da9e691f513e37dbef85
tarou652 <rrrr20050307@gmail.com>
2022-08-23 11:42:29 +0900

    時計（仮）の追加

---
+++ ちいさなまち/Assets/scripts/Gamescene/Tokei.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+using System;
+using TMPro;
+
+public  class Tokei : MonoBehaviourPunCallbacks
+{
+    public int hour, minite, second;
+    private Text text;
+    //ExitGames.Client.Photon.Hashtable roomHash;
+    //private itibyou byou;
+    //private timekimeru timekimeru;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var roomHash = timekimeru.roomHash;
+
+        // //var player = PhotonNetwork.PlayerList;
+        // //Array.Sort(player);
+        // // p1 = player[0];
+
+
+        text = GetComponent<Text>();
+
+        // byou = gameObject.AddComponent<itibyou>();
+
+
+        //byou.Init(() =>
+        //{
+        //    hour = (int)roomHash["hour"];
+        //    minite = (int)roomHash["minite"];
+        //    second = (int)roomHash["second"];
+        //    // hour = p1.Gethour();
+        //    //  minite = p1.Getminute();
+        //    //second = p1.GetScond();
+
+        //});
+        //byou.Play();
+
+        //hour = 6;
+        //if (PhotonNetwork.IsMasterClient)
+        //{
+
+
+        //    //var hashtable = new ExitGames.Client.Photon.Hashtable();
+        //    //hashtable["Score"] = 0;
+        //    //hashtable["Message"] = "こんにちは";
+        //    //PhotonNetwork.LocalPlayer.SetCustomProperties(hashtable);
+
+        //    var byou = gameObject.AddComponent<itibyou>();
+        //    byou.Init(() =>
+        //    {
+
+
+        //        second++;
+
+        //        if (second == 60) { second = 0; minite++; PhotonNetwork.LocalPlayer.Addminute(minite); }
+        //        if (minite == 60) { minite = 0; hour++; PhotonNetwork.LocalPlayer.Addhour(hour); PhotonNetwork.LocalPlayer.Addminute(minite); }
+        //        if (hour == 24) { hour = 0; PhotonNetwork.LocalPlayer.Addhour(hour); }
+        //        PhotonNetwork.LocalPlayer.Addsecond(second);
+        //        text.text = $"{hour}" + "時" + $"{minite}" + "分" + $"{second}" + "秒";
+        //    });
+        //    byou.Play();
+        //}
+    }
+    public override void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+    {
+        object value = null;
+        //変更のあったプロパティに"Time"が含まれているならtimeを更新
+        if (propertiesThatChanged.TryGetValue("hour", out value))
+        {
+            hour = (int)value;
+        }
+        if (propertiesThatChanged.TryGetValue("minite", out value))
+        {
+            minite = (int)value;
+        }
+        if (propertiesThatChanged.TryGetValue("second", out value))
+        {
+            second = (int)value;
+        }
+    }
+    //public   int GetScore(this Player player)
+    // {
+    //   return (player.CustomProperties[second] is int score) ? score : 0;
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = $"{hour}" + "時" + $"{minite}" + "分" + $"{second}" + "秒";
+    }
+    
+}
---
+++ ちいさなまち/Assets/scripts/Gamescene/timekimeru.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+using System;
+
+public class timekimeru : MonoBehaviourPunCallbacks
+{
+   public static ExitGames.Client.Photon.Hashtable roomHash;
+    public  int hour, minite, second;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var player = PhotonNetwork.PlayerList;
+        Array.Sort(player);
+        var p1 = player[0];
+        roomHash = new ExitGames.Client.Photon.Hashtable();
+        roomHash.Add("hour", hour);
+        roomHash.Add("minite", minite);
+        roomHash.Add("second", second);
+        PhotonNetwork.CurrentRoom.SetCustomProperties(roomHash);
+        if (p1==PhotonNetwork.LocalPlayer)
+        {
+            Debug.Log("ˆê”Ô‚Å‚·");
+            
+            //var hashtable = new ExitGames.Client.Photon.Hashtable();
+            //hashtable["Score"] = 0;
+            //hashtable["Message"] = "‚±‚ñ‚É‚¿‚Í";
+            //PhotonNetwork.LocalPlayer.SetCustomProperties(hashtable);
+
+            var byou = gameObject.AddComponent<itibyou>();
+            byou.Init(() =>
+            {
+
+
+                second++;
+
+                if (second == 60) { second = 0; minite++; roomHash["minite"]=minite; }
+                if (minite == 60) { minite = 0; hour++; roomHash["hour"]=hour; roomHash["minite"] = minite; }
+                if (hour == 24) { hour = 0; roomHash["hour"] = hour; }
+                roomHash["second"] = second;
+                PhotonNetwork.CurrentRoom.SetCustomProperties(roomHash);
+            });
+            byou.Play();
+
+
+        }
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
