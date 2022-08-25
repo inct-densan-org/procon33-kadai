c938aacc941f08444a5d4eb34b847e5a52fad563
tarou652 <rrrr20050307@gmail.com>
2022-08-25 09:28:35 +0900

    感染したら感染範囲の拡大

--- ちいさなまち/Assets/scripts/Gamescene/Infection2.cs
+++ ちいさなまち/Assets/scripts/Gamescene/Infection2.cs
@@ -7,16 +7,27 @@ public class Infection2 : MonoBehaviourPunCallbacks
     private bool cooltime;
     public static bool infected = false;
     private int infectionProbability=1;
+    private CircleCollider2D collider2;
+    public GameObject kansenhani;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //kansenhani = transform.Find("kansenhani").gameObject;
+       collider2 = this.GetComponent<CircleCollider2D>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (infected == true)
+        {
+            collider2.radius = 1f;
+        }
+        else
+        {
+            collider2.radius = 0.5f;
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
