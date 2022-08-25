9677674564fde2d38d03bda0fd48365a58af23de
Tin-HoNDa <hondasuzuto@gmail.com>
2022-08-25 10:39:42 +0900

    病院のランダム配置

--- ちいさなまち/Assets/scripts/Gamescene/TileController.cs
+++ ちいさなまち/Assets/scripts/Gamescene/TileController.cs
@@ -8,6 +8,8 @@ using System;
 
 public class TileController : MonoBehaviour
 {
+    public GameObject hospitalPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +19,21 @@ public class TileController : MonoBehaviour
         if (p1 == PhotonNetwork.LocalPlayer) 
         {
             var tilemap = GetComponent<Tilemap>();
-            var position = new Vector3Int(0, 0, 0);
-            for (int i = 0; i < 27; i++)
-            {
-                int rnd = UnityEngine.Random.Range(0, 27);
-                // タイルの種類を表す配列を用意、一個づつについて座標を割り振る。
-            }
+            var position = new Vector3(0, 0, 0);
+            // 病院の場所を決定
+            var rnd = UnityEngine.Random.Range(3, 5);
+            //if (rnd == 0) position = new Vector3(-12, -12, 0); // 左下
+            //else if (rnd == 1) position = new Vector3(0, -12, 0); // 下
+            //else if (rnd == 2) position = new Vector3(12, -12, 0); // 右下
+            if (rnd == 3) position = new Vector3(12, 0, 0); // 右
+            else if (rnd == 4) position = new Vector3(12, 12, 0); // 右上
+            //else if (rnd == 5) position = new Vector3(0, 12, 0); // 上
+            //else if (rnd == 6) position = new Vector3(-12, 12, 0); // 左上
+            //else if (rnd == 7) position = new Vector3(-12, 0, 0); // 左
+
+            Instantiate(hospitalPrefab, position, Quaternion.identity);
+            
+
         }
     }
 
