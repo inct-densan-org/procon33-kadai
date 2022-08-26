c880402dd3003aa5fe7b917c897b0d9e31ab3a0b
JOSSH-JORSTAR <103988288+JOSSH-JORSTAR@users.noreply.github.com>
2022-08-26 15:41:57 +0900

    ゲームオーバー画面外殻

---
+++ ちいさなまち/Assets/scripts/Gamescene/situation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class situation : MonoBehaviour
+{ 
+ public GameObject text;
+  
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+
+            text.SetActive(true);
+        }
+    }
+
+}
