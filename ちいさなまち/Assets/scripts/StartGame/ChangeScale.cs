d07df587c140639bfe0c51da278de0f654dd2652
SHO800 <86473181+SHO800@users.noreply.github.com>
2022-09-20 09:10:22 +0900

    微修正

--- ちいさなまち/Assets/scripts/StartGame/ChangeScale.cs
+++ ちいさなまち/Assets/scripts/StartGame/ChangeScale.cs
@@ -6,29 +6,28 @@ public class ChangeScale : MonoBehaviour
 {
     public float movingTime = 0.05f;
     public float[] widthTargets = new float[4];
-    private float widthTarget;
     private RectTransform rectTransform;
     private float scaleYTarget;
     private Vector2 velocity = Vector2.zero;
 
-    void Start() {
-        rectTransform = gameObject.GetComponent<RectTransform>();
-
+    void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+        scaleYTarget = 0f;
     }
 
     public void Move(int status)
     {
         //非表示0, サーバーセレクター1, ルール2, 設定3
-        widthTarget = widthTargets[status];
         if (status > 0){
-            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, widthTarget);
-            scaleYTarget = 1f;
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, widthTargets[status]);
+            scaleYTarget = 1;
         }else{
-            scaleYTarget = 0f;
+            scaleYTarget = 0;
         }
     }
     void Update()
     {
+
         float scaleY = Mathf.SmoothDamp(transform.localScale.y, scaleYTarget, ref velocity.y, movingTime);
         transform.localScale = new Vector2(1f, scaleY);
     }
