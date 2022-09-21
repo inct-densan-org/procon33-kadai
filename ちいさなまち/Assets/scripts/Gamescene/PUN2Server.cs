b28f8cc24916251af8ba7e666bf3119a3805892e
tarou652 <rrrr20050307@gmail.com>
2022-09-21 10:41:53 +0900

    バグの修正

--- ちいさなまち/Assets/scripts/Gamescene/PUN2Server.cs
+++ ちいさなまち/Assets/scripts/Gamescene/PUN2Server.cs
@@ -10,28 +10,92 @@ using System.Collections.Generic;
 // MonoBehaviourPunCallbacksを継承して、PUNのコールバックを受け取れるようにする
 public class PUN2Server : MonoBehaviourPunCallbacks
 {
-    public static bool isStart,ii;
+    public static bool isStart, ii;
     public static GameObject clone;
     public GameObject gamestart, wait;
     public TextMeshProUGUI joinnum;
     public static int isman;
     public static ExitGames.Client.Photon.Hashtable roomHash;
     public static Player localplayer;
-    public  bool issee;
-    private bool a,b;
+    public bool issee;
+    private bool a, b;
     public static int playernum;
-    private int f,localplayernum;
+    private int f, localplayernum;
     private GameObject[] tagObjects;
     private void Start()
     {
         gamestart.SetActive(false);
         wait.SetActive(true);
-        b = true;
+
         localplayer = PhotonNetwork.LocalPlayer;
         localplayernum = localplayer.ActorNumber;
         var player = PhotonNetwork.PlayerList;
         var p1 = player[0];
+       
 
+        if (p1 == PhotonNetwork.LocalPlayer)
+        {
+            Customproperties.custam();
+        }
+        Customproperties.mycustom(localplayernum);
+    }
+
+    // マスターサーバーへの接続が成功した時に呼ばれるコールバック
+
+    // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
+
+    public void Update()
+    {
+
+        issee = isStart;
+        tagObjects = GameObject.FindGameObjectsWithTag("Player");
+        playernum = PhotonNetwork.PlayerList.Length;
+        if (isStart == false)
+        {
+
+
+
+
+            joinnum.text = "参加人数　" + $"{playernum}" + "/8　　";
+
+        }
+        if (Input.GetKey(KeyCode.Space) && isStart == false)
+        {
+            
+            photonView.RPC(nameof(IsStart), RpcTarget.All);
+
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+        }
+        if (isStart == true && a == false)
+        {
+
+            gamestart.SetActive(true);
+            wait.SetActive(false);
+            a = true;
+            clone.SetActive(true);
+
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+
+            var tagObjects = GameObject.FindGameObjectsWithTag("Player").Length;
+            // for (int i = 0; i < tagObjects; i++) Debug.Log($"{i + 1}" + ";" + Customproperties.Getplayerinf(i + 1));
+            for (int i = 0; i < tagObjects; i++) Debug.Log($"{i + 1}" + ";" + Infection2.GetPlayerinf(i + 1));
+            var npcobj = GameObject.FindGameObjectsWithTag("NPC");
+            var npcobjnum = GameObject.FindGameObjectsWithTag("NPC").Length;
+            foreach (GameObject item in npcobj)
+            {
+                var j = item.name;
+                Debug.Log($"{j}" + ";" + Customproperties.GetNPCinf(j));
+            }
+
+        }
+    }
+
+    [PunRPC]
+    public void IsStart()
+    {
+        isStart = true;
         isman = Random.Range(0, 2);
         if (isman == 1)
         {
@@ -41,58 +105,18 @@ public class PUN2Server : MonoBehaviourPunCallbacks
         {
             clone = PhotonNetwork.Instantiate("woman", new Vector3(20, 15, -1), Quaternion.identity);
         }
-        if (p1 == PhotonNetwork.LocalPlayer)
-        {
-            Customproperties.custam();
-        }
-        Customproperties.mycustom(localplayernum);
     }
-
-    // マスターサーバーへの接続が成功した時に呼ばれるコールバック
-    
-    // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
-    
-    public void Update()
-    {
-        issee = isStart;
-        tagObjects = GameObject.FindGameObjectsWithTag("Player");
-        playernum = tagObjects.Length;
-        if (isStart == false&&b==true) 
-        {
-            joinnum.text = "参加人数　" + $"{playernum}" + "/8　　";
-        }
-        if (Input.GetKey(KeyCode.Space) && isStart == false && b == true)
-        {
-            photonView.RPC(nameof(IsStart), RpcTarget.All);
-        
-            PhotonNetwork.CurrentRoom.IsOpen = false;
-        }
-        if (isStart == true && a == false)
-        {
-            gamestart.SetActive(true);
-            wait.SetActive(false);
-            a = true;
-            clone.SetActive(true);
-        
-        }
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            var tagObjects = GameObject.FindGameObjectsWithTag("Player").Length;
-            // for (int i = 0; i < tagObjects; i++) Debug.Log($"{i + 1}" + ";" + Customproperties.Getplayerinf(i + 1));
-            for (int i = 0; i < tagObjects; i++) Debug.Log($"{i + 1}" + ";" + Infection2.GetPlayerinf(i+1));
-            var npcobj = GameObject.FindGameObjectsWithTag("NPC");
-            var npcobjnum = GameObject.FindGameObjectsWithTag("NPC").Length;
-            foreach (GameObject item in npcobj)
-            {
-                var j = item.name;
-                Debug.Log($"{j}" + ";" + Customproperties.GetNPCinf(j));
-            }
-        }
-    }
-    
     [PunRPC]
-    public void IsStart()
+    public void playermake()
     {
-        isStart = true;
+        isman = Random.Range(0, 2);
+        if (isman == 1)
+        {
+            clone = PhotonNetwork.Instantiate("man", new Vector3(20, 15, -1), Quaternion.identity);
+        }
+        else
+        {
+            clone = PhotonNetwork.Instantiate("woman", new Vector3(20, 15, -1), Quaternion.identity);
+        }
     }
 }
