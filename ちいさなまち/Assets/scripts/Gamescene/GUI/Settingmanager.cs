116eec76baa828ef85640769bba26d46ad849bb2
tarou652 <rrrr20050307@gmail.com>
2022-09-28 16:28:01 +0900

    マップの更新

--- ちいさなまち/Assets/scripts/Gamescene/GUI/Settingmanager.cs
+++ ちいさなまち/Assets/scripts/Gamescene/GUI/Settingmanager.cs
@@ -6,11 +6,13 @@ using TMPro;
 using UnityEngine.UI;
 public class Settingmanager : MonoBehaviourPunCallbacks
 {
+    public TMP_InputField inputField;
     public GameObject SettingSence,Camera,menu, slider;
     private AudioSource AudioSource;
     // Start is called before the first frame update
     void Start()
     {
+        inputField = inputField.GetComponent<TMP_InputField>();
         AudioSource = Camera.gameObject.GetComponent<AudioSource>();
         AudioSource.volume = ClientData.bgmVolume;
         slider.GetComponent<Slider>().value = ClientData.bgmVolume;
@@ -32,5 +34,9 @@ public class Settingmanager : MonoBehaviourPunCallbacks
         // ���y�̉��ʂ��X���C�h�o�[�̒l�ɕύX
         ClientData.bgmVolume = newSliderValue;
         AudioSource.volume = ClientData.bgmVolume;
+    }
+    public void changename()
+    {
+        PhotonNetwork.LocalPlayer.NickName = $"{inputField.text}";
     }
 }
